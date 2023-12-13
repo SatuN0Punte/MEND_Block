996058db8ce4b94c078095c5a0bc725ae8a9a039
Punyaphat <117330884+SatuN0Punte@users.noreply.github.com>
2023-12-13 14:26:10 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import { UserContextProvider } from './context/UserContext';
+import {Rouutes, Route} from "react-router-dom";
+import "./index.css";
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+<UserContextProvider>
+<Routes>
+<Route puth"/" element= {<Layout/>}>
+<Route index"/" element= {index/>}>
+
+</Route>
+</Routes>
+</UserContextProvider>
+}
+
+export default App
---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,8 @@
+import { createContext, useState } from "react";
+export const UserContext = createContext({});
+export const UserContextProvider = ({ children}) => {
+    const [userInfo, setUserInfo] = useState({});
+}
+return (
+    
+)
