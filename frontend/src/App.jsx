a6fb6f7784f61d6632ac60f080034d6956b08513
Punyaphat <117330884+SatuN0Punte@users.noreply.github.com>
2023-12-13 15:29:56 +0700

    Hlep

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,28 @@
 
 import './App.css';
 import { UserContextProvider } from './context/UserContext';
-import {Rouutes, Route} from "react-router-dom";
-import "./index.css";
-import { BrowserRouter } from 'react-router-dom';
+import { Rouutes, Route } from "react-router-dom";
+import indexPage from "./pages/indexPage";
+import LoginPage from "./pages/LoginPage";
+import RejisterPage from "./pages/RejisterPage";
+import CreatePage from "./pages/CreatePage";
+import PostPage from "./pages/PostPage";
+import EditPage from "./pages/EditPage";
 
 function App() {
-<UserContextProvider>
-<Routes>
-<Route puth"/" element= {<Layout/>}>
-<Route index"/" element= {index/>}>
+  <UserContextProvider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<indexPage />}>
+          <Route path="login" element={<LoginPage />}>
+            <Route path="rejister" element={<RejisterPage />}>
+              <Route path="create" element={<CreatePage />}>
+                <Route path="post" element={<PostPage />}>
+                  <Route path="edit" element={<EditPage />}>
 
-</Route>
-</Routes>
-</UserContextProvider>
+                  </Route>
+                </Routes>
+              </UserContextProvider>
 }
 
 export default App
